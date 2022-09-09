87d07b276e569b513e56f486f67e3a2f5e4df3b7
vlad <vladbelyvskij@gmail.com>
2022-09-09 23:54:23 +0300

    64

---
+++ task001/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
